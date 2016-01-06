afd74ac8f568d218fc4db85bf34427bbf0f1cfb5
Gustavo Fortunato Puga <gustavo.fortunatopuga@gmail.com>
2016-01-05 22:46:58 -0200

    commit inicial

---
+++ Check_calculadora.c
@@ -0,0 +1,39 @@
+#include <check.h>
+#include "calculadora.h"
+
+START_TEST(test_soma)
+{
+    ck_assert_int_eq(calculadora('+',1,1), 2);
+    ck_assert_int_eq(calculadora('+',-2,-2), -4);
+    ck_assert_int_eq(calculadora('+',10,-10), 0);
+}
+END_TEST
+
+START_TEST(test_subtracao)
+{
+    ck_assert_int_eq(calculadora('-',1,-1), 0);
+    ck_assert_int_eq(calculadora('-',-2,-2), 0);
+    ck_assert_int_eq(calculadora('-',10,-10), 20);
+}
+END_TEST
+
+START_TEST(test_multiplicacao)
+{
+    ck_assert_int_eq(calculadora('*',1,-1), -1);
+    ck_assert_int_eq(calculadora('*',-2,-2), 4);
+    ck_assert_int_eq(calculadora('*',10,-10), -100);
+}
+END_TEST
+
+START_TEST(test_divisao)
+{
+    ck_assert_int_eq(calculadora('/',1,-1), -1);
+    ck_assert_int_eq(calculadora('/',-2,-2), 1);
+    ck_assert_int_eq(calculadora('/',10,-10), -1);
+}
+END_TEST
+
+int main(void)
+{
+     return 0;
+}
